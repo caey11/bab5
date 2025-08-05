f69d4a99d4bd5ef72258bb102840efda0cbac87a
caey11 <akuica64@gmail.com>
2025-08-04 21:35:23 -0700

    Add files via upload

---
+++ Bab5/src/Bab5.java
@@ -0,0 +1,7 @@
+public static void main(String[] args) {
+Integer sebelum = 20;
+Integer sesudah = 0;
+System.out.println("Sebelum Increment : " + sebelum);
+sesudah = ++sebelum;
+System.out.println("Sesudah Increment : " + sesudah);
+}
